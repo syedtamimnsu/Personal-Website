c41939fad2fc04d7c8c19dd19a5f439c6d2a5c99
syed-tamim <syed.tamim@northsouth.edu>
2025-03-19 03:57:18 +0600

    project added

---
+++ script.js
@@ -0,0 +1,8 @@
+//typing animation
+
+var typed = new Typed(".typing",{
+    strings: ["web Desiner","web Developer", "Graphic Designer", "YouTuber"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
